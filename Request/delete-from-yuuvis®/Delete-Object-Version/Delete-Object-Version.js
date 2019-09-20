72cbc936a16f3b01140aac29a4a6929d5928cbbd
james lockwood <ksjjl05@gmail.com>
2019-09-20 13:49:02 -0500

    rename folders

---
+++ Request/delete-from-yuuvis®/Delete-Object-Version/Delete-Object-Version.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+var key = ""
+var baseUrl = "https://api.yuuvis.io/"
+var oid = "" //object id
+var vid = "" //version id
+
+
+function createDeleteRequest(objectId,versionNr) {
+  return{
+    method: 'DELETE',
+    uri: baseUrl + 'dms/objects/'+objectId+'/versions/'+ versionNr,
+    headers: {
+      'Ocp-Apim-Subscription-Key': key
+    }
+  }
+}
+
+function executeRequest(request_object){
+  request.delete(request_object, function callback(err, httpResponse, body) {
+    if(err) throw err;
+    else {
+      console.log(httpResponse.statusCode)
+      console.log(body)
+    }
+  })
+}
+
+var simpleSearchRequest = createDeleteRequest(oid,vid)
+executeRequest(simpleSearchRequest)
