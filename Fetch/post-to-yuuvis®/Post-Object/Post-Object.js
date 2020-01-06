d4192aa77c512aaf5b57510415169b0b498a707a
Max Lüdemann <luedemann@optimal-systems.de>
2020-01-06 10:49:42 +0100

    simplify axios/fetch import request

--- Fetch/post-to-yuuvis®/Post-Object/Post-Object.js
+++ Fetch/post-to-yuuvis®/Post-Object/Post-Object.js
@@ -1,38 +1,34 @@
-const fetch = require("node-fetch");
 const fs = require("fs");
+const axios = require("axios");
+var FormData = require("form-data");
 
-const key = "your API key here";
-const baseUrl = "https://api.yuuvis.io";
 
-//Import Metadata
-const headersImport = {
-    "Content-Type": "application/json",
-    "Ocp-Apim-Subscription-Key": key
-};
-const optionsImport = {
-    method: "POST",
-    body: fs.createReadStream("metadata.json"),
-    headers: headersImport
-};
+const key = "";
+const baseUrl = "https://api.yuuvis.io/";
+
+const data = new FormData();
+
+const metadataBody = fs.createReadStream("metadata.json");
+data.append("data", metadataBody);
+
+const contentBody = fs.createReadStream("text.txt");
+data.append("cid_63apple", contentBody);
+
+const formDataHeaders = data.getHeaders();
+formDataHeaders["Ocp-Apim-Subscription-Key"] = key;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 (async() => {
     try {
-        const response = await fetch(baseUrl + "/dms-core/objects/", optionsImport);
-        const data = await response.json();
-        const oid = data.objects[0].properties["system:objectId"].value;
-        console.log("new oid: ", oid);
-
-        //Add contentStreamAllowed
-        const headersContentUpdate = {
-            "Ocp-Apim-Subscription-Key": key,
-            "Content-Disposition": "attachment; filename=\"test.txt\""
-        };
-        const optionsContentUpdate = {
-            method: "POST",
-            body: fs.createReadStream("test.txt"),
-            headers: headersContentUpdate
-        };
-        console.log(await fetch(baseUrl + "/dms-core/objects/" + oid + "/contents/file", optionsContentUpdate));
+        const response = await axios.post(baseUrl + "dms-core/objects", data, {headers: formDataHeaders});
+        console.log(response.status);
+        const objectId = response.data.objects[0].properties["system:objectId"].value;
+        console.log(objectId);
+        await sleep(1000);
     } catch (error) {
         console.log(error);
     }
