1d0a91896a3449a1cdd5ca18c39c6fc6d8e81abc
Antje Oelschlägel <oelschlaegel@optimal-systems.de>
2020-10-12 15:57:39 +0200

    new endpoint

---
+++ Axios/get-from-yuuvis®/Get-Admin-Metrics/Get-Admin-Metrics.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+const key = "your API key here";
+const baseUrl = "https://api.yuuvis.io";
+
+axios.get(
+    baseUrl + "/admin/metrics", {
+        headers: {
+            "Ocp-Apim-Subscription-Key": key
+        }
+    }).then(response => {
+    console.log(response);
+    }).catch(error => console.log(error));
